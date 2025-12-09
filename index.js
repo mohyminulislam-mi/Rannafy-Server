db86685785080bfecc0a24b52c022bbbfa1ad74e
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-12-09 18:49:28 +0600

    create register data into database api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import dependencies
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Create app
@@ -12,8 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-const uri =
-  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@mohyminulislam.uwhwdlk.mongodb.net/?appName=Mohyminulislam`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@mohyminulislam.uwhwdlk.mongodb.net/?appName=Mohyminulislam`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,8 +25,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+    await client.connect();
+    const database = client.db("RannaFy");
+    const usersCollection = database.collection("users");
+
+    // users data into Database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.createdAt = new Date();
+
+      // exists user checking
+      const userExists = await usersCollection.findOne({ email: user.email });
+      if (userExists) {
+        return res.send({ message: "User Exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      console.log('result', result);
       
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
